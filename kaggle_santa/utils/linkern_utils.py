872896e53d60674de5ca7ca1a64a000895a9c783
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-11-30 13:25:28 +0300

    added samples

--- kaggle_santa/utils/linkern_utils.py
+++ kaggle_santa/utils/linkern_utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sympy as sympy
+import os
 
 
 def write_tsp(cities, filename, name='traveling-santa-2018-prime-paths'):
@@ -24,7 +25,7 @@ def read_tour(filename):
     return tour
 
 
-def score_tour(tour):
+def score_tour(tour, cities):
     df = cities.reindex(tour + [0]).reset_index()
     primes = list(sympy.primerange(0, len(cities)))
     df['prime'] = df.CityId.isin(primes).astype(int)
@@ -38,17 +39,35 @@ def write_submission(tour, filename):
     pd.DataFrame({'Path': list(tour) + [0]}).to_csv(filename, index=False)
 
 
-if __name__ == '__main__':
+def make_submission(cities, date='20181130', n_subm=1,
+                    tour_file='../data/linkern.tour',
+                    submission_dir='../submissions'):
+    tour = read_tour(tour_file)
+    submission_file = os.path.join(submission_dir, f'submission_{date}_{n_subm}.csv')
+    write_submission(tour, submission_file)
+    return score_tour(tour, cities)
 
+
+def build_full_tsp():
     # multiply coordinates by 1000, here's the reason:
     # concorde's EUC_2D norm rounds the distances between cities
     # to the nearest integer (source) whereas competition metric doesn't;
     # this significantly hurts quality as we get closer to TSP optimum".
     cities = pd.read_csv('../data/cities.csv', index_col=['CityId'])
-    # cities1k = cities * 1000
-    # write_tsp(cities1k, '../data/cities1k.tsp')
+    cities1k = cities * 1000
+    write_tsp(cities1k, '../data/cities1k.tsp')
 
-    date, n = '20181129', 1
-    tour = read_tour('../data/linkern.tour')
-    write_submission(tour, f'../submissions/submission_{date}_{n}.csv')
-    print(score_tour(tour))
+
+def build_sample_tsp(n_sample=100):
+    cities = pd.read_csv('../data/cities.csv', index_col=['CityId'])
+    cities1k = cities * 1000
+    cities1k_sample = cities1k.sample(n=n_sample, random_state=42) \
+                              .reset_index() \
+                              .drop(columns='CityId')
+    cities1k_sample.index.name = 'CityId'
+    write_tsp(cities1k_sample, f'../data/cities1k_{n_sample}.tsp')
+
+
+if __name__ == '__main__':
+    cities = pd.read_csv('../data/cities.csv', index_col=['CityId'])
+    print(make_submission(cities))
