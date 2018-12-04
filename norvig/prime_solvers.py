9d82ba9d9dfd766b3f4f13dc7c12f9c5e40b8480
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-12-04 13:35:17 +0300

    misc changes

--- norvig/prime_solvers.py
+++ norvig/prime_solvers.py
@@ -1,3 +1,22 @@
-from utils.gen_utils import *
+from prime_utils import *
 
+
+def ptour_2opt_solver(ptour, pcities, primes):
+    for base_index in range(9, len(ptour), 10):
+        probe_index = get_close_prime(ptour, primes, base_index=base_index)
+        if probe_index:
+            is_better = is_alt_better(ptour, pcities, base_index, probe_index)
+            if is_better:
+                print(f'base_index:{base_index} probe_index:{probe_index} FIND IMPROVEMENT!')
+            else:
+                print(f'base_index:{base_index} probe_index:{probe_index} ...')
+        else:
+            print(f'base_index:{base_index} no probe_index ...')
+
+
+if __name__ == '__main__':
+    pcities = read_pcities(filename='../data/samples/csv_samples/cities1k_100.csv')
+    ptour = read_ptour(filename='../data/samples/tour_samples/linkern_cities1k_100.tour')
+    primes = get_primes()
+    ptour_2opt_solver(ptour, pcities, primes)
 
--- norvig/utils/prime_utils.py
+++ norvig/utils/prime_utils.py
@@ -94,19 +94,7 @@ def is_alt_better(ptour, pcities, base_index, probe_index, penalty_rate=.1):
     return old_distance + penalty > new_distance
 
 
-def ptour_2opt_solver(ptour, pcities, primes):
-    for base_index in range(9, len(ptour), 10):
-        probe_index = get_close_prime(ptour, primes, base_index=base_index)
-        if probe_index:
-            is_better = is_alt_better(ptour, pcities, base_index, probe_index)
-            if is_better:
-                print(f'base_index:{base_index} probe_index:{probe_index} FIND IMPROVEMENT!')
-            else:
-                print(f'base_index:{base_index} probe_index:{probe_index} ...')
-
-
 if __name__ == '__main__':
     pcities = read_pcities()
     ptour = read_ptour()
     primes = get_primes()
-    ptour_2opt_solver(ptour, pcities, primes)
