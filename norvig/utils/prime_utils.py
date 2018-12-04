1c28d577a30cad46d6ee99bdf0011bca935bf138
Ilya Rudyak <ilyarudyak@yahoo.com>
2018-12-04 13:29:15 +0300

    done ptour solver (1st draft)

--- norvig/utils/prime_utils.py
+++ norvig/utils/prime_utils.py
@@ -71,15 +71,42 @@ def get_close_prime(ptour, primes, base_index=0, nodes=3):
     if ptour[base_index] in primes:
         return None
 
-    for i in range(base_index+1, base_index+nodes):
+    limit = min(base_index+nodes, len(ptour))
+    for i in range(base_index+1, limit):
         if ptour[i] in primes:
             return i
 
     return None
 
 
+def is_alt_better(ptour, pcities, base_index, probe_index, penalty_rate=.1):
+
+    prev_to_base = pcities[ptour[base_index-1]]
+    base = pcities[ptour[base_index]]
+    next_to_base = pcities[ptour[base_index+1]]
+    probe = pcities[ptour[probe_index]]
+    next_to_probe = pcities[ptour[probe_index+1]]
+
+    old_distance = distance(prev_to_base, base) + distance(probe, next_to_probe)
+    penalty = distance(base, next_to_base) * penalty_rate
+    new_distance = distance(prev_to_base, probe) + distance(next_to_probe, base)
+
+    return old_distance + penalty > new_distance
+
+
+def ptour_2opt_solver(ptour, pcities, primes):
+    for base_index in range(9, len(ptour), 10):
+        probe_index = get_close_prime(ptour, primes, base_index=base_index)
+        if probe_index:
+            is_better = is_alt_better(ptour, pcities, base_index, probe_index)
+            if is_better:
+                print(f'base_index:{base_index} probe_index:{probe_index} FIND IMPROVEMENT!')
+            else:
+                print(f'base_index:{base_index} probe_index:{probe_index} ...')
+
+
 if __name__ == '__main__':
     pcities = read_pcities()
     ptour = read_ptour()
     primes = get_primes()
-    plot_ptour(ptour, pcities)
+    ptour_2opt_solver(ptour, pcities, primes)
